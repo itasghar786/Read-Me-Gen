ba640cbcbaa0c6533d59975fe366a4c2d998d727
asghar sheikh <itasghar786@gmail.com>
2020-03-08 22:45:41 -0400

    Up dated files

--- index.js
+++ index.js
@@ -1,79 +1,102 @@
-const inquirer = require  ("inquirer");
-const fs = require ("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 
-const writeFileAsync = util.promisify(fs.writeFile);
-const qustions =[
+const appendFileAsync = util.promisify(fs.appendFile);
+const readFileAsync = util.promisify(fs.readFile);
+const qustions = [
 
-        {
-     type: "input",
-     name: "username",
+  {
+    type: "input",
+    name: "username",
     message: "what is your username?"
-     },
-           
- {
-    type: "input",
-    name: "proName",
-    message: "what is your projects name?"
-                },
+  },
 
-                {
+  {
     type: "input",
-    name: "smallProject",
-      message: "write a small discription of your project."
-                    },
-                    {
-     type: "input",
+    name: "title",
+    message: "what is your projects name?"
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "write a small discription of your project."
+  },
+  {
+    type: "input",
     name: "npm-i",
     message: "what command should be run for dependicies?"
-                        },
-                        {
-   type: "input",
-     name: "runTest",
-     message: "what command should be run to run tests?"
-                            },
+  },
+  {
+    type: "input",
+    name: "runTest",
+    message: "what command should be run to run tests?"
+  },
 
-                            {
-        type: "input",
-      name: "repo",
+  {
+    type: "input",
+    name: "repo",
     message: "what should the user know about useing the repo?"
-                                },
-
-                                {
-     type: "input",
-      name: "contRepo",
-     message: "what does the user need to know about contributing to the repo?"
-                                    },
-                                ]
-                                   
-    
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "what license does the user need?"
+  },
 
 
-                                function init() {
-                                    inquirer.prompt(qustions).then(function ({username}){
-
-                                      const url = `https://api.github.com/users/${username}`;
-                                      axios.get(url).then (function(res){
-                                        console.log(res);
-                                        
-                                    })});
-
-
-                                     // const html = generateHTML(response);
-
-                                      //return writeFileAsync ("generateMarkdown.js",html )
-                                    }
-
-                                    //console.log("");
+]
 
 
 
-                                    
 
-                                
-                                
-                                init();
+function init() {
+  inquirer.prompt(qustions).then(function (response) {
+
+    const url = `https://api.github.com/users/(response.username)`;
+    axios.get(url).then(function (res) {
+      console.log(res);
+      const data = {
+        username: response.username,
+        title: response.title,
+        description: response.description,
+        tableOfContents: response.tableOfContents,
+        installation: response.installation,
+        usage: response.repo,
+        tests: response.runTest,
+        license: response.license,
+        contributing: response.contributing,
+        name: res.data.login,
+        email: "itasghar786.com",
+        profilePic: res.data.avatar_url,
+      }
+
+      //const html = generateHTML(data;
+
+      return appendFileAsync("generateMarkdown.js", data.join('\n'));
+    })
+      .then(function () {
+        return readFileAsync("generateMarkdown.js", "utf8");
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+
+  });
+
+}
+
+//console.log("");
+
+
+
+
+
+
+
+init();
 
 //promtUser().then (function(answers)
-            
+
